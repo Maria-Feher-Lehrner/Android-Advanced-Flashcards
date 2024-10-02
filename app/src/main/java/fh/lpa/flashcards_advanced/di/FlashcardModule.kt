726868327e80669aa27e742e8a703d45dc134ed3
Maria Feher-Lehrner <maria@feher.at>
2024-10-02 16:54:59 +0200

    1) Added Repository and Database to FlashcardModule.kt

--- app/src/main/java/fh/lpa/flashcards_advanced/di/FlashcardModule.kt
+++ app/src/main/java/fh/lpa/flashcards_advanced/di/FlashcardModule.kt
@@ -1,5 +1,26 @@
 package fh.lpa.flashcards_advanced.di
 
-/*val appModule = module {
+import androidx.room.Room
+import fh.lpa.flashcards_advanced.entity.WordpairsAppDatabase
+import fh.lpa.flashcards_advanced.repository.VocabularyRepository
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
 
-}*/
+val appModule = module {
+
+    //TODO: implement viewModelOf
+
+    singleOf(::VocabularyRepository)
+
+    //TODO: evtl. implementieren HttpClient & ApiAccess
+
+    single<WordpairsAppDatabase> {
+        val database = Room.databaseBuilder(
+            get(),
+            WordpairsAppDatabase::class.java,
+            "FlashcardDB"
+        ).fallbackToDestructiveMigration()
+            .build()
+        database
+    }
+}
