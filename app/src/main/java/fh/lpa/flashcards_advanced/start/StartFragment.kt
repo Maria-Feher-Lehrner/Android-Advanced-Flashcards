d807d4937986e423dd667b12d1a09063908334a2
Maria Feher-Lehrner <maria@feher.at>
2025-01-12 18:36:48 +0100

    Fixed ID mismatch

--- app/src/main/java/fh/lpa/flashcards_advanced/start/StartFragment.kt
+++ app/src/main/java/fh/lpa/flashcards_advanced/start/StartFragment.kt
@@ -1,20 +1,40 @@
 package fh.lpa.flashcards_advanced.start
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import fh.lpa.flashcards_advanced.R
 
-class StartFragment : Fragment(R.layout.fragment_start){
+class StartFragment : Fragment(R.layout.fragment_start) {
 
     private lateinit var _inputGer: EditText
     private lateinit var _inputFra: EditText
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _inputGer = view.findViewById(R.id.etGermanWord)
-        _inputFra = view.findViewById(R.id.etFrenchWord)
+        _inputGer = view.findViewById(R.id.eT_language1)
+        _inputFra = view.findViewById(R.id.eT_language2)
+
+        //val saveButton = view.findViewById<Button>(R.id.btn_save)
+        val quizButton = view.findViewById<Button>(R.id.btn_quiz)
+        val editButton = view.findViewById<Button>(R.id.btn_edit)
+
+        editButton.setOnClickListener{
+            findNavController().navigate(StartFragmentDirections.actionStartFragmentToListViewFragment())
+        }
 
     }
 
