0a0b0c101800dd6da853e0b6b2b2f3ae9779932b
Maria Feher-Lehrner <maria@feher.at>
2025-01-13 19:35:06 +0100

    Registered StartViewModel with koin

--- app/src/main/java/fh/lpa/flashcards_advanced/di/FlashcardModule.kt
+++ app/src/main/java/fh/lpa/flashcards_advanced/di/FlashcardModule.kt
@@ -1,22 +1,22 @@
 package fh.lpa.flashcards_advanced.di
 
 import androidx.room.Room
-
 import org.koin.dsl.module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import fh.lpa.flashcards_advanced.vocabDetail.DetailViewModel
 import fh.lpa.flashcards_advanced.entity.WordpairsAppDatabase
 import fh.lpa.flashcards_advanced.vocabList.ListViewModel
 import fh.lpa.flashcards_advanced.repository.VocabularyRepository
-import org.koin.android.ext.koin.androidContext
-
-import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.viewModelOf
-
+import fh.lpa.flashcards_advanced.start.StartViewModel
 
 val appModule = module {
 
     viewModelOf(::DetailViewModel)
     viewModelOf(::ListViewModel)
+    viewModel { StartViewModel(get()) }
+
 
     singleOf(::VocabularyRepository)
 
